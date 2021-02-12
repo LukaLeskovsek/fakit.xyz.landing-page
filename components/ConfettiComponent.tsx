ceb06f3d6495b7108051208b1e1b542f95d2d312
LukaLeskovsek <luka.leskovsek@dlabs.si>
2021-02-12 15:43:03 +0100

    Added clipboard support; design fix; toast message

--- components/ConfettiComponent.tsx
+++ components/ConfettiComponent.tsx
@@ -1,13 +1,24 @@
 import Confetti from 'react-confetti';
 import { useWindowSize } from '@react-hook/window-size'//import useWindowSize from './WindowSizeHook';
+import { useToasts } from 'react-toast-notifications'
 import { useEffect, useState } from 'react';
-const ConfettiComponent = () => {
+import useClippy from 'use-clippy';
+
+interface Props {
+    GenerateType: string;
+}
+const ConfettiComponent = (props: Props) => {
     const [width, height] = useWindowSize();
     const [loaded, setLoaded] = useState(false);
+    const { addToast } = useToasts();
+    const [clipboard, setClipboard] = useClippy();
+
     useEffect(() => {
         console.log('loaded')
         setLoaded(true);
-    })
+        setClipboard('https://fakit.xyz/magic?type='+props.GenerateType);
+        addToast('Link copied to clipboard', { appearance: 'success' })  
+    }, [])
 
     return(
         loaded ? <Confetti width={width} height={height} recycle={false} numberOfPieces={288}/> : <div/>
--- pages/magic.tsx
+++ pages/magic.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import { NextPageContext } from 'next'
+import React, { useEffect } from 'react';
 import Link from 'next/link';
 import tw, { styled, css } from 'twin.macro';
 import ConfettiComponent from './../components/ConfettiComponent';
+import { ToastProvider } from 'react-toast-notifications'
 
 interface Props {
     type?: string;
 }
 
 const Container = styled.div`
-  ${tw`min-h-screen flex items-center justify-center bg-gray-100`}
+  ${tw`min-h-screen flex items-center justify-center content-center bg-gray-100`}
 `;
 
 const GenerateText = styled.div`
@@ -20,36 +20,37 @@ const Card = styled.div`
     ${tw`max-w-md px-5 py-8 bg-white rounded-lg shadow-lg`}`;
 
 const CTA = styled.div`
-    ${tw`inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-2xl font-medium rounded-md text-white bg-indigo-600 tracking-widest
+    ${tw`content-center text-center py-2 px-4 border border-transparent shadow-sm text-2xl font-medium rounded-md text-white bg-indigo-600 tracking-widest
     hover:bg-purple-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 `}
 `;
 
-export default class Page extends React.Component<Props> {
-    static async getInitialProps({ query }: NextPageContext) {
-        const type = query.type;
+const Page = (props: Props) => {
+return( 
+    <main>
+        <ToastProvider autoDismiss={true}>
+            <Container>
+                <Card>
+                    <ConfettiComponent GenerateType={props.type}/>
+                    <GenerateText>Fuck {props.type}!</GenerateText>
+                    <CTA>
+                        <Link href={`/`}>
+                            <a>Generate again!</a>
+                        </Link>
+                    </CTA>
+                </Card>
+            </Container>
+        </ToastProvider>
+    </main>
+    )
+}
 
-        return {
-            type
-        };
-    }
-  
-    render() {
-        return( 
-            <main>
-                <Container>
-                    <Card>
-                        {<ConfettiComponent />}
-                        <GenerateText>Fuck {this.props.type}!</GenerateText>
-                        <p/>
-                        <CTA>
-                            <Link href={`/`}>
-                                <a>Generate again!</a>
-                            </Link>
-                        </CTA>
-                    </Card>
-                </Container>
-            </main>
+Page.getInitialProps = async ({query}) => {
 
-        )
-    }
-  }
+    const type = query.type;
+
+    return {
+        type
+    };
+}
+
+export default Page;
