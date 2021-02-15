aa6d70bf8087f44f4395552f3f879f05aadaab00
LukaLeskovsek <luka.leskovsek@dlabs.si>
2021-02-15 14:28:35 +0100

    Meta Image - Dynamic. Cleanup,

--- pages/magic.tsx
+++ pages/magic.tsx
@@ -15,37 +15,27 @@ import {
     WhatsappShareButton
 } from "react-share";
 import { motion } from "framer-motion"
-
-
-
 interface Props {
     type?: string;
 }
 
-const Container = styled.div`
-  ${tw`min-h-screen flex items-center justify-center content-center bg-gray-100`}
-`;
+const Container = styled.div`${tw`min-h-screen flex items-center justify-center content-center bg-gray-100`}`;
 
-const GenerateText = styled.div`
-    ${tw`text-6xl font-bold p-5 py-20`}
-`;
+const GenerateText = styled.div`${tw`text-6xl font-bold p-5 py-20`}`;
 
-const Card = styled.div`
-    ${tw`max-w-md px-5 pt-8 pb-4 bg-white rounded-lg shadow-lg`}
-    
-
-    `
-    ;
+const Card = styled.div`${tw`max-w-md px-5 pt-8 pb-4 bg-white rounded-lg shadow-lg`}`;
 
 const CTA = styled.div`
     ${tw`content-center text-center py-2 px-4 border border-transparent shadow-sm text-2xl font-medium rounded-md text-white bg-indigo-600 tracking-widest
     hover:bg-purple-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 `}
 `;
 
-const ShareSection = styled.div`
-    ${tw`table w-full pt-4 text-center`}
-`;
+const ShareSection = styled.div`${tw`table w-full pt-4 text-center`}`;
 const Page = (props: Props) => {
+
+    const full_image_name = 'https://fakit.xyz/fakit.generated.'+props.type+'.png';
+    const share_url = 'https://fakit.xyz/magic?type='+props.type;
+    
     return (
         <main>
             <Head>
@@ -58,19 +48,19 @@ const Page = (props: Props) => {
 
                 <meta itemProp="name" content="F*ck IT" />
                 <meta itemProp="description" content="It’s time to acknowledge your frustration. Maybe you’re feeling helpless or wishing there was some way to express yourself. Or you’re just plain angry and need a word or two to vent. We feel you. We’ve got that word for you. And it starts with F and rhymes with kuck… we mean luck." />
-                <meta itemProp="image" content="https://fakit.xyz/fakit.generated.yeah.png" />
+                <meta itemProp="image" content={full_image_name} />
 
 
                 <meta property="og:url" content="https://fakit.xyz" />
                 <meta property="og:type" content="website" />
                 <meta property="og:title" content="F*ck IT" />
                 <meta property="og:description" content="It’s time to acknowledge your frustration. Maybe you’re feeling helpless or wishing there was some way to express yourself. Or you’re just plain angry and need a word or two to vent. We feel you. We’ve got that word for you. And it starts with F and rhymes with kuck… we mean luck." />
-                <meta property="og:image" content="https://fakit.xyz/fakit.generated.yeah.png" />
+                <meta property="og:image" content={full_image_name} />
 
                 <meta name="twitter:card" content="summary_large_image" />
                 <meta name="twitter:title" content="F*ck IT" />
                 <meta name="twitter:description" content="It’s time to acknowledge your frustration. Maybe you’re feeling helpless or wishing there was some way to express yourself. Or you’re just plain angry and need a word or two to vent. We feel you. We’ve got that word for you. And it starts with F and rhymes with kuck… we mean luck." />
-                <meta name="twitter:image" content="https://fakit.xyz/fakit.generated.yeah.png" />
+                <meta name="twitter:image" content={full_image_name} />
             </Head>
             <ToastProvider autoDismiss={true}>
                 <Container>
@@ -79,7 +69,7 @@ const Page = (props: Props) => {
                         <motion.div
                             initial={{ scale: 0.0 }}
                             animate={{ scale: [1.3, 0.8, 1] }}
-                            transition={{ duration: 0.5, delay:0.5 }}>
+                            transition={{ duration: 0.5, delay: 0.5 }}>
                             <GenerateText>Fuck {props.type}!</GenerateText>
                         </motion.div>
 
@@ -99,19 +89,19 @@ const Page = (props: Props) => {
 
                         <ShareSection>
 
-                            <TwitterShareButton url='https://fakit.xyz/' title='F*CK! It’s time to acknowledge your frustration.'>
+                            <TwitterShareButton url={share_url} title='F*CK! It’s time to acknowledge your frustration.'>
                                 <TwitterIcon size={32} round={true} />
                             </TwitterShareButton>
 
-                            <FacebookShareButton url='https://fakiy.xyz/' quote='F*CK! It’s time to acknowledge your frustration.' >
+                            <FacebookShareButton url={share_url}  quote='F*CK! It’s time to acknowledge your frustration.' >
                                 <FacebookIcon size={32} round={true} />
                             </FacebookShareButton>
 
-                            <ViberShareButton url='https://fakit.xyz/' title='F*CK! It’s time to acknowledge your frustration.'>
+                            <ViberShareButton url={share_url}  title='F*CK! It’s time to acknowledge your frustration.'>
                                 <ViberIcon size={32} round={true} />
                             </ViberShareButton>
 
-                            <WhatsappShareButton url='https://fakit.xyz/' title='F*CK! It’s time to acknowledge your frustration.'>
+                            <WhatsappShareButton url={share_url}  title='F*CK! It’s time to acknowledge your frustration.'>
                                 <WhatsappIcon size={32} round={true} />
                             </WhatsappShareButton>
 
